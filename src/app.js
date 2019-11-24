a7ba1e071b93d6c9c19af51180a41b7d07675129
breakfastmeansbreakfast <77744c@gmail.com>
2019-11-24 18:28:10 +0000

    refactoring

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 
 const strings = require('../src/lib/strings');
+const numbers = require('../src/lib/numbers');
 
+// strings
 app.get('/strings/hello/:string', (req, res) => {
   res.status(200).json({ result: `${strings.sayHello(req.params.string)}` });
 });
@@ -19,5 +21,37 @@ app.get('/strings/lower/:string', (req, res) => {
 app.get('/strings/first-characters/:string', (req, res) => {
   res.status(200).json({ result: `${strings.firstCharacters(req.params.string, req.query.length)}` });
 });
+
+// numbers
+app.get('/numbers/add/:n1/and/:n2', (req, res) => {
+  const isNumeric = (string) => { return /^\d+$/.test(string); };
+  if (isNumeric(req.params.n1) || isNumeric(req.params.n2)) {
+    res.status(200).json({ result: numbers.add((+req.params.n1), (+req.params.n2)) });
+  } else {
+    res.status(400).json({ error: 'Parameters must be valid numbers.' });
+  }
+});
+
+// numbers
+app.get('/numbers/add/:n1/and/:n2', (req, res) => {
+  const isNumeric = (string) => { return /^\d+$/.test(string); };
+  if (isNumeric(req.params.n1) || isNumeric(req.params.n2)) {
+    res.status(200).json({ result: numbers.add((+req.params.n1), (+req.params.n2)) });
+  } else {
+    res.status(400).json({ error: 'Parameters must be valid numbers.' });
+  }
+});
+
+// numbers
+app.get('/numbers/subtract/:n2/from/:n1', (req, res) => {
+  const isNumeric = (string) => { return /^\d+$/.test(string); };
+  if (isNumeric(req.params.n1) || isNumeric(req.params.n2)) {
+    res.status(200).json({ result: numbers.subtract((+req.params.n1), (+req.params.n2)) });
+  } else {
+    res.status(400).json({ error: 'Parameters must be valid numbers.' });
+  }
+});
+
+
 
 module.exports = app;
